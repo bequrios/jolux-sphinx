dad7bd02e4cb393d55e30f242d81afa95fb08add
Benedikt Hitz <bhitz@gmx.net>
2024-06-28 09:18:41 +0200

    try sparql links

--- doc/conf.py
+++ doc/conf.py
@@ -3,6 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('./extensions'))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -15,7 +19,8 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser',
+              'sparql_link']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
---
+++ doc/extensions/sparql_link.py
@@ -0,0 +1,38 @@
+from docutils import nodes
+from docutils.parsers.rst import Directive
+from sphinx.util.docutils import SphinxDirective
+from sphinx.util.nodes import nested_parse_with_titles
+
+class SparqlQueryNode(nodes.General, nodes.Element):
+    pass
+
+
+def visit_sparql_query_node_html(self, node):
+    self.body.append(self.starttag(node, 'div', CLASS='sparql-query'))
+    self.body.append('<pre><code>')
+
+
+def depart_sparql_query_node_html(self, node):
+    self.body.append('</code></pre>')
+    query = node['query'].replace('\n', ' ').replace('"', '\\"')
+    endpoint = 'https://your-sparql-endpoint.com/sparql'  # Replace with your SPARQL endpoint
+    url = f'{endpoint}?query={query}'
+    link_html = f'<a href="{url}" target="_blank">Execute Query</a>'
+    self.body.append(link_html)
+    self.body.append('</div>')
+
+
+class SparqlQueryDirective(SphinxDirective):
+    has_content = True
+
+    def run(self):
+        query = '\n'.join(self.content)
+        node = SparqlQueryNode(query=query)
+        self.add_name(node)
+        nested_parse_with_titles(self.state, self.content, node)
+        return [node]
+
+
+def setup(app):
+    app.add_node(SparqlQueryNode, html=(visit_sparql_query_node_html, depart_sparql_query_node_html))
+    app.add_directive('sparql-query', SparqlQueryDirective)
